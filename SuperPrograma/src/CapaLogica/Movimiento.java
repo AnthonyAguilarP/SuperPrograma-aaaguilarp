a46282abbd5dc16cf457dd8bbb1de18567812c61
Anthony <Anthonyaguilarparrales2005@gmail.com>
2022-09-03 04:00:42 +0200

    Segunda Tarea

---
+++ SuperPrograma/src/CapaLogica/Movimiento.java
@@ -0,0 +1,21 @@
+package CapaLogica;
+
+public class Movimiento {
+    
+    public static String velocidadMRU(String distancia, String tiempo){
+        try{
+            if(Double.parseDouble(tiempo)==0.0)return "No es posible una division entre cero";
+            return Double.parseDouble(distancia)/Double.parseDouble(tiempo)+"";
+        }catch(Exception e){
+            return e.getMessage();
+        }
+    }
+    public static String hmaxProyectil(String velocidad, String angulo){
+        try{
+            Double radianes=Double.parseDouble(angulo)*0.01745329;
+            return (Math.pow(Double.parseDouble(velocidad), 2)*Math.pow(Math.sin(radianes), 2))/(9.8*2)+"";
+        }catch(Exception e){
+            return e.getMessage();
+        }
+    }
+}
